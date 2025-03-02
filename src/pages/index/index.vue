9253583769ed5f1d1f09511dc54eb0d2af75f0b6
raozhaizhu <raozhaizhu@gmail.com>
2025-03-02 14:42:58 +0800

    完成前台分类

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,28 +1,38 @@
 <script setup lang="ts">
 import Navbar from '@/pages/index/components/Navbar.vue'
-import { getHomeBanner } from '@/services/home'
-import type { BannerItem } from '@/types/home'
+import { getHomeBanner, getHomeCategory } from '@/services/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
+const categoryList = ref<CategoryItem[]>([])
 
 const getHomeBannerData = async () => {
   const res = await getHomeBanner()
-  console.log('响应数据', res)
   bannerList.value = res.result
 }
 
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategory()
+  console.log('前台分类', res)
+  categoryList.value = res.result
+}
+
 onLoad(() => {
-  getHomeBannerData()
+  getHomeBannerData(), getHomeCategoryData()
 })
 </script>
 
 <template>
   <Navbar />
   <RzzSwiper :list="bannerList" />
+  <CategoryPanel :list="categoryList" />
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
